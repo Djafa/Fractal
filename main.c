79b4e03bb0174251a3afbf97456ddde4ce7479d7
damien-vaneberck <damien.vaneberck@student.uclouvain.be>
2016-04-30 15:05:50 +0200

    Lecture fichier + debut makefile

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "fractal.h"
+#include "libfractal/fractal.h"
 
-int main()
-{
-    /* TODO */
+#define LENGTH_LINE 1000 // taille definie pour une ligne du fichier
 
+int main (int argc, char *argv[]) {
+	char line [LENGTH_LINE] = ""; // string servant a acceuillir la ligne lue
+    FILE* file = NULL;
+    file = fopen(argv[1], "r");
+    
+    if (file != NULL) {
+		while (fgets(line, LENGTH_LINE, file) != NULL) {
+			printf("%s", line);
+		}
+	}
+	
+	
     return EXIT_SUCCESS;
 }
