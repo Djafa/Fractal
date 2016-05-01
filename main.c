047d434832dfea8ae61ea177dcb60dc4d69127ce
roussieau <julian.roussieau@student.uclouvain.be>
2016-05-01 15:49:34 +0200

    Calcul d'une fractale

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "fractal.h"
+#include "./libfractal/fractal.h"
 
 int main()
 {
-    /* TODO */
+    //Test des calculs
+	const char *name = "projet";
+	struct fractal *f = fractal_new(name, 4 , 4, 2, 1);
+	int w = fractal_get_width(f);
+	int h = fractal_get_height(f);
 
-    return EXIT_SUCCESS;
+	//Calcule de chaque pixel
+	for(int x =0; x<w; x++)
+	{
+		for(int y =0; y<h ;y++)
+		{
+			fractal_compute_value(f,x,y);
+		}
+	}
+	//On écrit la fractale
+	write_bitmap_sdl(f, fractal_get_name(f));
 }
