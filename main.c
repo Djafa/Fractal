c3a87f463692100204aba33fc47c0313262671d6
roussieau <julian.roussieau@student.uclouvain.be>
2016-05-06 18:17:43 +0200

    Ajout du lancement par ligne de commande

--- main.c
+++ main.c
@@ -8,49 +8,70 @@
 #include "main.h"
 #include "prodAndCons.h"
 
-#define LENGTH_LINE 1000 // taille definie pour une ligne du fichier
-#define BUFFER 20
+const int genAll;
 
-
-
+ 
 int main (int argc, const char *argv[]) {
-	int nombreDeThread = 4;
-	int size1 = strlen("./fract_inputs/01input_testavg.txt")+1;
-	char *str1 = (char *)malloc(sizeof(char)*(size1));
-	if (str1 == NULL)
-		printf("Erreur malloc STR1\n");
-	strncpy(str1,"./fract_inputs/01input_testavg.txt", size1);
-	int size2 = strlen("./fract_inputs/02input_fewbig.txt")+1;
-	char *str2 = (char *)malloc(sizeof(char)*(size2));
-	if(str2 == NULL)
-		printf("Erreur malloc STR2\n");
-	strncpy(str2,"./fract_inputs/02input_fewbig.txt", size2);
-	int size3 = strlen("./fract_inputs/03input_manysmall.txt")+1;
-	char *str3 = (char *)malloc(sizeof(char)*(size3));
-	if(str3 == NULL)
-		printf("Erreur malloc STR3\n");
-	strncpy(str3,"./fract_inputs/03input_manysmall.txt", size3);
+	int start = 1;
+	const int nombreDeThread;
+	int consoleInput = 0; 
 
-	int error; 
+	//Configuration de la génération
+	if(argc > 2 && strmcp(argv[1],'-d') == 0){
+		genAll = 1;
+		start++;
+	}
+	else if(argc > 3 && strmcp(argv[2],'-d')){
+		genAll = 1;
+		start++;
+	}
+	else 
+		genAll = 0;
 
-	
-	error = initStack(BUFFER, nombreDeThread);
+	//Configuration du nombre de threads
+	if(argc > 2 && strmcp(argv[1],'--maxthreads') == 0){
+		nombreDeThread = atoi(argv[2]);
+		start += 2;
+	}
+	else if(argc > 3 && strmcp(argv[2],'--maxthreads') == 0){
+		nombreDeThread = atoi(argv[3]);
+		start += 2;
+	}
+	else 
+		nombreDeThread = 4;
+
+	//On check si il y a un - en argument
+	for(int i = start; i<argc-1; i++){
+		if(strmcp(argv[i],'-') == 0)
+			consoleInput = 1;
+	}
+
+	int nombreDeFichier = argv - 2 - start - consoleInput;
+	char *tab[nombreDeFichier];
+	for(int i = start, int j = 0; j<nombreDeFichier; i++, j++){
+		if(strmcp(argv[i],'-') == 0)
+			i++;
+		int size = strlen(argv[i])+1;
+		tab[j] = (char *)malloc(sizeof(char)*(size));
+		if(str == NULL)
+			printf("Erreur malloc \n");
+		strncpy(tab[j],argv[i], size1);
+	}
+
+	int error = initStack(BUFFER, nombreDeThread);
 	if(error !=0)
 		return EXIT_FAILURE;
-	int nbrFile = 3;
-	pthread_t threadsP[nbrFile];
-	pthread_t threadsC[nombreDeThread];
-	char *arg [nbrFile];
-	arg [0] = str1;
-	arg [1] = str2;
-	arg [2] = str3;   
+
+	//Création des threads
+	pthread_t threadsP[nombreDeFichier];
+	pthread_t threadsC[nombreDeThread]; 
 
 	printf("Il y a %d threads de calcul \n", nombreDeThread);
-	printf("Il y a %d fichiers \n", nbrFile);
+	printf("Il y a %d fichiers \n", nombreDeFichier);
 
 	//Création des threads de lecture (Producteurs)
 	for(int i = 0;i<nbrFile ; i++){
-		error = pthread_create(&(threadsP[i]), NULL, &producteur,arg[i]);
+		error = pthread_create(&(threadsP[i]), NULL, &producteur,tab[i]);
 		if(error != 0)
 			printf("%d \n", error);
 		printf("Création du thread de lecture numéro %d \n", i);
@@ -64,6 +85,10 @@ int main (int argc, const char *argv[]) {
 		printf("Création du thread de consommation numéro %d \n", i);
 	}
 
+
+	//Console
+
+
 	//Ce block permet d'attendre la fin de la lecture des fichiers
 	for(int i = 0; i<nbrFile ; i++){
 		error = pthread_join(threadsP[i], NULL);
@@ -72,14 +97,10 @@ int main (int argc, const char *argv[]) {
 		printf("Le producteur numéro %d à fini \n", i);
 	}
 
-	//free(str1);
-	//free(str2);
-	//free(str3);
-
 	//Ouverture du double fond 
 	kill(nombreDeThread);
 
-	//Join consommateur
+	//On attend la fin des consommateurs
 	for(int i = 0; i<nombreDeThread ; i++){
 		error = pthread_join(threadsC[i], NULL);
 		if(error != 0)
@@ -88,10 +109,9 @@ int main (int argc, const char *argv[]) {
 	}
 
 	//Destruction
-
 	printf("Et le gagnant est : %s \n", fractal_get_name(maxF));
-	write_bitmap_sdl(maxF, fractal_get_name(maxF));
-
+	write_bitmap_sdl(maxF, argv(argc-1) + ".bmp");
+	destroy();
 	pthread_mutex_destroy(&best);
 	fractal_free(maxF);
 	return EXIT_SUCCESS;
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#define LENGTH_LINE 1000
+#define BUFFER 20
+
+
+
+#endif
